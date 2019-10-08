ea71b56d29e5458c3cc2f6b12e1e1cae7bd6f21f
Jose Montero <jams4991@gmail.com>
2019-10-07 22:51:36 -0300

    add rabbitMQ to proyect

--- beer-service/src/index.js
+++ beer-service/src/index.js
@@ -2,27 +2,28 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 4000
 app.use(express.json())
-// const RabbitService = require('./rabbit-service')
-// const rabbitService = new RabbitService('microService01')
-const {RedisHandler} = require('./handlers')
+const {RedisHandler, RabbitMQHandler} = require('./handlers')
 const redisHandler = new RedisHandler()
-
+const rabbitService = new RabbitMQHandler('beerService')
 
 app.get('/beers', async (req, res) =>  {
     let beers = await redisHandler.getBeers()
     res.send(beers)
-//    if (rabbitService.isConnected()) {
-//       rabbitService.sendMessage('Greetings from the Micro Service #01', 'microService02')
-//       res.send('we already send a message to the micro service number 02')
-//    } else {
-//       await rabbitService.connectServer()
-//       if (rabbitService.isConnected()) {
-//          rabbitService.sendMessage('Greetings from the Micro Service #01 another try', 'microService02')
-//          res.send('we send a message to the micro service number 02 after a second reconnect to rabbitmq server')
-//       } 
-//       else
-//          res.send('rabbit is not connected, please try again in a few moments')
-//    }
+})
+
+app.get('/beers/:beerID/boxPrice', async (req, res) => {
+    if(!req.params.beerID)
+        res.send('ERROR EN GET')
+    else {
+        let beer = await redisHandler.getBeer(req.params.beerID)
+        let quantity = (req.query.quantity) ? req.query.quantity : 1
+        let price = beer.boxPrice * quantity
+        if (req.query.currency && rabbitService.isConnected()) {
+            rabbitService.sendMessage('Greetings from the Micro Service #01', 'currencyService') 
+            res.send(`${price} CLP`)             
+        } else
+            res.send(`${price} CLP`)
+    }
 })
 
 app.get('/beers/:beerID', async (req, res) =>  {
--- currency-service/src/index.js
+++ currency-service/src/index.js
@@ -2,11 +2,9 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000
 app.use(express.json())
-// const RabbitService = require('./rabbit-service')
-// const rabbitService = new RabbitService('microService01')
-const {CurrencyLayerHandler} = require('./handlers')
+const {CurrencyLayerHandler, RabbitMQHandler} = require('./handlers')
 const currencyHandler = new CurrencyLayerHandler()
-
+const rabbitService = new RabbitMQHandler('currencyService')
 
 app.get('/beers', async (req, res) =>  {
     let beers = await redisHandler.getBeers()
